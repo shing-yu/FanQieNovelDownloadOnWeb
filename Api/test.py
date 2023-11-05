87be807ff766f66864ff59756100e2a056de68ac
xing-yv <xing_yv@outlook.com>
2023-11-06 00:49:38 +0800

    注释测试代码，防止缺库弱警告

--- Api/test.py
+++ Api/test.py
@@ -1,6 +1,6 @@
-from easywebdav import Client
-
-
-client = Client("pan.xingyv.site", port=443, username='weiwei', password='gsw091005', path='dav', protocol='https')
-
-client.ls("/")
+# from easywebdav import Client
+#
+#
+# client = Client("pan.xingyv.site", port=443, username='weiwei', password='gsw091005', path='dav', protocol='https')
+#
+# client.ls("/")
